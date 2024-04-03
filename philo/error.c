be5ce02753223e1894f68ff4e7327472eaa6dd29
Szabina Korbai <skorbai@student.hive.fi>
2024-04-03 10:07:43 +0300

    Added function destroy mutexes

--- philo/error.c
+++ philo/error.c
@@ -6,7 +6,7 @@
 /*   By: skorbai <skorbai@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 13:10:57 by skorbai           #+#    #+#             */
-/*   Updated: 2024/04/02 15:49:26 by skorbai          ###   ########.fr       */
+/*   Updated: 2024/04/03 09:46:41 by skorbai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,14 @@ void	free_philos(t_philo **philos, t_params *params)
 	free(philos);
 }
 
-/*void	destroy_mutexes(t_philo **philos, int i)
+void	destroy_mutexes(t_philo **philos, int i)
 {
-	
-}*/
+	int	j;
+
+	j = 0;
+	while (j < i)
+	{
+		pthread_mutex_destroy(&philos[j]->own_fork);
+		j++;
+	}
+}
