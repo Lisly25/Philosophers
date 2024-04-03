6bbde4bcd840a943305c2eeb2df7ee462d8a5e9a
Szabina Korbai <skorbai@student.hive.fi>
2024-04-03 14:56:40 +0300

    Rearranged starting pattern, might not be final yet

--- philo/simulation_utils.c
+++ philo/simulation_utils.c
@@ -6,7 +6,7 @@
 /*   By: skorbai <skorbai@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 11:23:35 by skorbai           #+#    #+#             */
-/*   Updated: 2024/04/03 12:12:45 by skorbai          ###   ########.fr       */
+/*   Updated: 2024/04/03 14:52:20 by skorbai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,18 +29,18 @@ void	set_start_pattern(t_philo *philo)
 	if (philo->philo_count % 2 == 0)
 	{
 		if (philo->nro % 2 == 0)
-			usleep(1);
-		else
 			return ;
+		else
+			usleep(1);
 	}
 	else
 	{
 		if (philo->nro == 0)
 			usleep(2);
 		else if (philo->nro % 2 == 0)
-			usleep(1);
-		else
 			return ;
+		else
+			usleep(1);
 	}
 }
 
@@ -55,7 +55,7 @@ void	go_to_eat(t_philo *philo)
 	while (i != eat_time)
 	{
 		usleep(500);
-		i += 500;
+		i = i + 500;
 	}
 }
 
@@ -66,10 +66,10 @@ void	go_to_sleep(t_philo *philo)
 
 	sleep_time = philo->time_to_sleep * 1000;
 	i = 0;
-	print_status(philo, "is eating");
+	print_status(philo, "is sleeping");
 	while (i != sleep_time)
 	{
 		usleep(500);
-		i += 500;
+		i = i + 500;
 	}
 }
